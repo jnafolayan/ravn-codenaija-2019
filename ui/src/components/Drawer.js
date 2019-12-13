f265269143b50281b7f0f3dcdc87e837062651f0
John Afolayan <jnafolayan@gmail.com>
2019-12-13 04:26:46 +0100

    prep for drawer

--- ui/src/components/Drawer.js
+++ ui/src/components/Drawer.js
@@ -1,8 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import Drawer from "../components/Drawer";
 
 const Wrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  
   .title {
     text-align: center;
     font-size: 18px;
--- ui/src/screens/Home.js
+++ ui/src/screens/Home.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Header from "../components/Header";
 import MapView from "../components/MapView";
 import Navbar from "../components/Navbar";
+import Drawer from "../components/Drawer";
 
 const Wrapper = styled.div`
   display: grid;
